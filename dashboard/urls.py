4c33674a02fdb044c1a1646236098900ba34fcd7
alison <alison.americo@gmail.com>
2018-11-19 23:51:50 -0300

    Import projeto

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import core.views
+
 
 urlpatterns = [
+    path('', core.views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
