2344e3260bea488b41a91b93c58d151e7b23c92e
alison <alison.americo@gmail.com>
2018-11-19 23:51:50 -0300

    Import projeto

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,42 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django_assertions import dj_assert_contains
+
+
+def test_status_code(client):
+    response = client.get('/')
+    assert 200 == response.status_code
+
+
+@pytest.mark.parametrize(
+    'content', [
+        'COK Store',
+        'Contato',
+        'Copyright',
+        'href="/contato/"',
+    ]
+)
+def test_home(client, content):
+    response = client.get('/')
+    dj_assert_contains(response, content)
+
+
+def test_contact_status_code(client):
+    response = client.get('/contato/')
+    assert 200 == response.status_code
+
+
+@pytest.mark.parametrize(
+    'content', [
+        'Rua Jataúba 161',
+        'Recife, PE',
+        '(81) 9 8888-8888',
+        'name@example.com',
+        'Segunda - Sexta: 9:00 às 17:00',
+    ]
+)
+def test_contact_content(client, content):
+    response = client.get('/contato/')
+dj_assert_contains(response, content)
+
+
--- dashboard/settings.py
+++ dashboard/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+from dj_database_url import parse as dburl
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,12 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sn3$v@mx$7x@161)5dpj8il#h=+m)2hp%@*g61m)5z17f+q!rg'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -73,11 +76,10 @@ WSGI_APPLICATION = 'dashboard.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+   'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
 }
 
 
@@ -103,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Recife'
 
 USE_I18N = True
 
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
